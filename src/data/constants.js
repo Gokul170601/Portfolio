001e112e9bf1f678748871292c444eeb97b6620f
Gokul170601 <gokulgokul6847@gmail.com>
2025-01-25 14:32:50 +0530

    init

---
+++ src/data/constants.js
@@ -0,0 +1,476 @@
+
+export const Bio = {
+  name: "Gokul",
+  roles: [
+    "Data Scientist",
+    "Data Analyst",
+    "AI/ML Engineer",
+    "GenAI Enthusiast",
+  ],
+  description: "I am a versatile professional with expertise in data science, AI, and machine learning.Passionate about innovation and continuous learning in AI, I am dedicated to delivering impactful results with a positive attitude and a growth mindset.",
+  github: "https://github.com/Gokul170601",
+  resume:
+    "https://drive.google.com/file/d/1JKhkYoFLWCMAcZaeXRmxOnBYxUV4QkD8/view",
+  linkedin: "https://www.linkedin.com/in/gokul-m-j17/",
+  insta: "https://www.instagram.com/gokul_gk_17/",
+  youtube: "https://youtube.com/@gokul_m_17?si=CfgNJKBzSJGWiSSf"
+};
+
+export const skills = [
+  {
+    title: "ML/DL/NLP",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Scikit-Learn",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/2560px-Scikit_learn_logo_small.svg.png",
+      },
+      {
+        name: "Tenserflow",
+        image:
+          "https://static-00.iconduck.com/assets.00/tensorflow-icon-1911x2048-1m2s54vn.png",
+      },
+      {
+        name: "PyTorch",
+        image:
+          "https://static-00.iconduck.com/assets.00/pytorch-icon-1694x2048-jgwjy3ne.png",
+      },
+      {
+        name: "Keras",
+        image:
+          "https://miro.medium.com/v2/resize:fit:600/1*DKu_54iqz6C-p6ndo7rO3g.png",
+      },
+      {
+        name: "NLTK",
+        image:
+          "https://miro.medium.com/v2/resize:fit:592/1*YM2HXc7f4v02pZBEO8h-qw.png",
+      },
+      {
+        name: "Spacy",
+        image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/SpaCy_logo.svg/220px-SpaCy_logo.svg.png",
+      },
+      {
+        name: "OpenCV",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/OpenCV_Logo_with_text.png/487px-OpenCV_Logo_with_text.png",
+      },
+      
+    ],
+  },
+  {
+    title: "Data Processing & Visualization",
+    skills : [
+      {
+        name: "Pandas",
+        image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHZd37oUzVXPHOsl-Ygg5hzYpZs7Djvk-vSw&s",
+      },
+      {
+        name: "Numpy",
+        image:
+        "https://cdn.worldvectorlogo.com/logos/numpy-1.svg",
+      },
+      {
+        name: "Spark",
+        image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Apache_Spark_logo.svg/250px-Apache_Spark_logo.svg.png",
+      },
+      {
+        name: "Kafka",
+        image:
+        "https://e7.pngegg.com/pngimages/630/547/png-clipart-kafka-vertical-logo-tech-companies-thumbnail.png",
+      },
+      {
+        name: "Seaborn",
+        image:
+        "https://user-images.githubusercontent.com/315810/92161415-9e357100-edfe-11ea-917d-f9e33fd60741.png",
+      },
+      {
+        name: "Matplotlib",
+        image:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/5/56/Matplotlib_logo.svg/300px-Matplotlib_logo.svg.png",
+      },
+      {
+        name: "Power BI",
+        image:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/2/20/Power_BI_logo.svg/70px-Power_BI_logo.svg.png",
+      },
+    ]
+  },
+  {
+    title: "Generative AI & AI Agents",
+    skills:[
+      {name: "Hugging_Face",
+        image:
+          "https://thomassimonini-huggy.static.hf.space/logo.png",
+      },
+      {
+      name: "Rasa",
+        image:
+          "https://info.rasa.com/hubfs/Rasa_January2020/Images/rasa-logo-60e441f8eadef13bea0cc790c8cf188b.svg",
+      },
+      {
+        name: "Langchain",
+        image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb2FZsSE8lCx255gSAAV4LyVLQr6PVravPCA&s",
+      },
+      {
+        name: "Groq",
+        image:
+        "https://raw.githubusercontent.com/RMNCLDYO/groq-ai-toolkit/main/.github/groq-logo.png",
+      },
+      {
+        name: "Ollama",
+        image:
+        "https://ollama.com/public/ollama.png",
+      },
+      {
+        name: "CrewAI",
+        image:
+        "https://cdn.prod.website-files.com/66cf2bfc3ed15b02da0ca770/66d07240057721394308addd_Logo%20(1).svg",
+      },
+      {
+        name: "Phidata",
+        image:
+        "https://mintlify.s3.us-west-1.amazonaws.com/phidata/logo/light.svg",
+      },
+    ]
+  },
+  {
+    title: "DataBases & VectorDB",
+    skills: [
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "SQLite",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/SQLite370.svg/382px-SQLite370.svg.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+      {
+        name: "ChromaDB",
+        image:
+        "https://miro.medium.com/v2/resize:fit:1044/1*0slU8hyYQ4edDptchVhQIg.png"
+      },
+      {
+        name: "Pinecone",
+        image:
+        "https://mma.prnewswire.com/media/2418074/Pinecone_Systems_Inc_Logo.jpg?p=facebook"
+      }
+    ],
+  },
+  {
+    title: "Web Development & Deployment",
+    skills: [
+      {
+        name: "Django",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlFFyY16N5NRhzoG24RqB7x8Ok2t8Bdgs-tuMsOBXS2Q&s",
+      },
+      {
+        name: "Flask",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Flask_logo.svg/1280px-Flask_logo.svg.png",
+      },
+      {
+        name: "Streamlit",
+        image:
+          "https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png",
+      },
+      {
+        name: "Docker",
+        image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJl4fp0SkQbTPU5ZxVl6AKWYuKCwM0gIhNtQ&s",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name:"AWS",
+        image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Amazon_Web_Services_Logo.svg/512px-Amazon_Web_Services_Logo.svg.png",
+      },
+      {
+        name: "FastAPI",
+        image:
+        "https://cdn.worldvectorlogo.com/logos/fastapi.svg",
+      }
+    ],
+  },
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Google Colab",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Google_Colaboratory_SVG_Logo.svg/800px-Google_Colaboratory_SVG_Logo.svg.png?20221103151432",
+      },
+      {
+        name:"Selenium",
+        image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWDtEPx8dJaNJ59Mvsmp6pAdg65diPQO5lXA&s",
+      },
+      {
+        name: "Beautiful Soup",
+        image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCt5bDNb3R50sYA9yNQW-cTxoofH6Ayi4xrQ&s",
+      }
+      
+    ],
+  },
+];
+
+
+export const experiences = [
+  
+  {
+    id: 0,
+    img: "https://www.infosys.com/content/dam/infosys-web/en/global-resource/18/springboard-logo.png",
+    role: "AI Intern",
+    company: "Infosys Springboard",
+    date: "Nov 2024 - Jan 2025",
+    desc: "I worked on Retrieval-Augmented Generation (RAG) model to improve knowledge retrieval by chunking, embedding, and storing data in a vector database, boosting response accuracy by 20%. Integrated LLMs for context-based responses and memory management for seamless interactions. Built agents to fetch information, cutting response times by 15%. Created a hospitality assistant with a FastAPI backend and intuitive frontend, handling 500+ daily queries.",
+    skills: [
+      "LangChain",
+      "Jina",
+      "ChromaDB",
+      "Groq",
+      "Mem0",
+      "CrewAI",
+      "FastAPI",
+    ],
+    doc: "",
+  },
+  {
+    id: 1,
+    img: "https://kmdigicommerce.com/wp-content/uploads/2024/08/KM-1536x1482.png",
+    role: "Data Scientist Intern",
+    company: "KM Digi Commerce Pvt Ltd.",
+    date: "Oct 2024- Nov 2024",
+    desc: "I developed a Conversational AI Chatbot using the Rasa framework, enabling users to access product details with 95% query accuracy. By optimizing database queries, I reduced response times by 30%, improving user satisfaction. Additionally, I automated web data extraction using Selenium and BeautifulSoup, processing over 5000 data points daily and storing them in structured databases, which enhanced data availability and reduced manual effort by 40%.",
+    skills: [
+      'Python',
+      'Pandas',
+      'MySQL',
+      'Rasa(Conversational AI)',
+      'NLP',
+      'Selenium',
+      'Beautiful Soup',
+    ],
+    doc: "",
+  },
+];
+ 
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQXyYKvvMg_bFcAlOn3gNaFTEffaLASj0WmA&s",
+    school: "Guvi - IIT Madras,chennai.",
+    date: "Jan 2024 - July 2024",
+    grade: "-",
+    desc: "Completed a Master's in Data Science, mastering Python from foundational to advanced concepts. Acquired expertise in machine learning, deep learning, and NLP through hands-on projects. Developed strong skills in data visualization and predictive modeling to solve real-world problems.",
+    degree: "Master's in Data Science",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPOSqWTh3YPQf01TIE83QCCD4OKplW2RSSuA&s",
+    school: "Jeppiaar Institute of Technology, Chennai",
+    date: "Sep 2021 - June 2023",
+    grade: "75%",
+    desc: "Completed an MBA with a specialization in HR and Business Analytics, gaining expertise in workforce management, data-driven decision-making, and strategic business insights. Developed strong analytical skills using tools like Excel and Power BI, and applied them to solve real-world business challenges.",
+    degree: "Master of Business Administration",
+  },
+  {
+    id: 2,
+    img: "https://upload.wikimedia.org/wikipedia/en/4/4c/Sri_Chandrasekharendra_Saraswathi_Viswa_Mahavidyalaya_logo.png",
+    school: "Sri Chandrasekharendra Saraswathi Viswa Mahavidyalaya, Kanchipuram",
+    date: "Sep 2018 - June 2021",
+    grade: "81%",
+    desc: "Completed a Bachelor of Commerce (B.Com),gaining a strong foundation in accounting, finance, business management, and economics. Developed essential skills in financial analysis, business communication, and strategic decision-making",
+    degree: "Bachelor of Commerce",
+  },
+];
+
+export const projects = [
+  {
+    id: 7,
+    title: "MediBot: AI-Driven Healthcare Assistant",
+    description:
+      "Developed a Medical Chatbot using Generative AI to answer healthcare-related queries by analyzing PDF documents (e.g., medical journals, reports). The system retrieves contextually relevant information using LangChain for AI workflows and Sentence Transformers for text embeddings, stored in Pinecone for efficient vector search. Built a user-friendly interface with Flask and enabled PDF processing with PyPDF.",
+    image:
+      "https://github.com/Gokul170601/Medical-Chatbot-Generative-AI/blob/main/result.png?raw=true",
+    tags: [
+      "Python",
+      "Flask",
+      "LangChain",
+      "Pinecone",
+      "Sentence Transformers",
+      "PyPDF",
+    ],
+    category: "Gen AI",
+    github: "https://github.com/Gokul170601/Medical-Chatbot-Generative-AI",
+    webapp: "https://github.com/Gokul170601/Medical-Chatbot-Generative-AI",
+  },
+  {
+    id: 6,
+    title: "GUVI-GPT Text Generator",
+    description:
+    "This project deploys a fine-tuned GPT-2 model on Hugging Face Spaces, featuring a Streamlit-based chatbot interface. It includes secure user authentication with encrypted passwords and stores user data in TiDB Cloud. The project combines advanced AI, secure authentication, and scalable data management for a user-friendly chatbot experience.",
+    image:
+      "https://github.com/Gokul170601/GUVI-GPT-Text-Generator/blob/main/result.png?raw=true",
+    tags: [
+      "Transformers",
+      "GPT-2",
+      "Hugging Face",
+      "TiDB Cloud",
+      "Streamlit",
+    ],
+    category: "Gen AI",
+    github: "https://github.com/Gokul170601/GUVI-GPT-Text-Generator",
+    webapp: "https://huggingface.co/spaces/Gokul1701/Guvi-GPT",
+  },
+  {
+    id: 5,
+    title: "Movie Recommender",
+    description:
+      "A Streamlit-based movie recommendation system that uses TF-IDF vectorization and cosine similarity to recommend movies based on the selected movie. The application also fetches movie details and posters from IMDb.",
+    image:
+      "https://github.com/Gokul170601/Movie-Recommender/blob/main/result.png?raw=true",
+    tags: [
+      "Pandas",
+      "Scikit-learn",
+      "IMDbPY",
+      "streamlit",
+      "Pickle",
+    ],
+    category: "Machine Learning",
+    github: "https://github.com/Gokul170601/Movie-Recommender/",
+    webapp: "https://imdb--movie--recommender--system.streamlit.app/",
+  },
+  {
+    id: 4,
+    title: "Resale Flat Prices Prediction",
+    description:
+      "This project predicts Singapore flat resale prices using HDB data and machine learning. After thorough preprocessing and model testing, I built a user-friendly Streamlit app for real-time predictions. Deployed on Render for easy access. Excited to empower users in the housing market!",
+    image:
+      "https://github.com/Gokul170601/Singapore-Resale-Flat-Prices-Prediction/blob/main/result.png?raw=true",
+    tags: [
+      "Python",
+      "Pandas",
+      "Scikit-Learn",
+      "Streamlit",
+      "Pickle",
+      "Render",
+    ],
+    category: "Machine Learning",
+    github: "https://github.com/Gokul170601/Singapore-Resale-Flat-Prices-Prediction",
+    webapp: "https://singapore-resale-flat-prices-prediction-03q3.onrender.com/",
+  },
+  {
+    id: 3,
+    title: "Industrial Copper Price Prediction",
+    description:
+      "This project about copper modeling, leveraging the power of machine learning to predict selling price and status of copper after the data preprocessing and feature engineering. I built a user-friendly Streamlit application that empowers users to interact with the models and explore copper data in a dynamic way.",
+    image:
+      "https://github.com/Gokul170601/Industrial-Copper-Modeling/blob/main/result.png?raw=true",
+    tags: [
+      "Python",
+      "Numpy",
+      "Pandas",
+      "Scikit-Learn",
+      "Pickle",
+      "Streamlit",
+    ],
+    category: "Machine Learning",
+    github: "https://github.com/Gokul170601/Industrial-Copper-Modeling",
+    webapp: "https://youtu.be/qsAPGlfTZ-k",
+  },
+  {
+    id: 2,
+    title: "Airbnb-Analysis",
+    description:
+      "Extracted Airbnb data from a JSON dataset using Python, transforming it into a structured DataFrame. Enhanced data quality through cleaning and organizing. Stored the data in a MySQL database using the MySQL Python connector. Developed an interactive Streamlit dashboard with dynamic Plotly visualizations, utilizing SQLAlchemy for querying..",
+    image:
+      "https://theappanalyst.com/assets/airbnb/banner.png",
+    tags: [
+      "Python",
+      "Pandas",
+      "MySQL",
+      "Streamlit",
+      "Plotly",
+    ],
+    category: "Data Analysis and Visualization",
+    github: "https://github.com/Gokul170601/Airbnb-Analysis",
+    webapp: "https://www.youtube.com/watch?v=3OYxfZl6BjY",
+  },
+  {
+    id: 1,
+    title: "Phonepe-Pulse-Data-Visualization",
+    description:
+      "This project involves cloning the PhonePe Pulse dataset using Git and then transforming it using Pandas. The transformed data is subsequently stored in a SQL database for querying. Additionally, an interactive dashboard is developed with Streamlit and Plotly, featuring geoplots for enhanced data exploration and insights.",
+    image:
+      "https://techcrunch.com/wp-content/uploads/2021/09/phonepe-pulse.jpg",
+    tags: [
+      "Python",
+      "Pandas",
+      "MySQL",
+      "Streamlit",
+      "Plotly",
+    ],
+    category: "Data Analysis and Visualization",
+    github: "https://github.com/Gokul170601/Phonepe-Pulse-Data-Visualization-and-Exploration",
+    webapp: "https://youtu.be/-PQhoSl6GGQ?si=Yp53qDwjJBTKIQR0",
+  },
+  {
+    id: 0,
+    title: "Youtube Channel Data Analysis",
+    description:
+      "This project about Building a simple dashboard or UI using Streamlit and retrieve YouTube channel data with the help of YouTube API. Stored the data in SQL database(warehousing), enabling querying of the data using SQL. Visualize the data within the Streamlit.",
+    image:
+      "https://i.ytimg.com/vi/ab8Bx3nvf3U/maxresdefault.jpg",
+    tags: [
+      "Python",
+      "Streamlit",
+      "YouTube API",
+      "SQL",
+    ],
+    category: "Data Analysis and Visualization",
+    github: "https://github.com/Gokul170601/YouTube-Data-Harvesting-and-Warehousing-using-SQL-and-Streamlit",
+    webapp: "https://youtu.be/qsHF35eShKk",
+  },
+];
